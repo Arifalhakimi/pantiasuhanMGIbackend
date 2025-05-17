d4c5ceb1fc01beae6655c04b2921d19eb07ecc02
Arifalhakimi <21104039@ittelkom-pwt.ac.id>
2025-05-18 00:54:05 +0700

    SAS

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "http://srv1155.hstgr.io/",
+  host: process.env.DB_HOST || "srv1155.hstgr.io/",
   user: process.env.DB_USER || "u344296107_arifal",
   password: process.env.DB_PASS || "Triplek041",
   database: process.env.DB_NAME || "u344296107_db_pantiasuhan",
